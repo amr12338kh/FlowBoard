4ac8da689d3a7ad8393e212b082720cfa981fdfa
Amr <amrkhaled12338@gmail.com>
2025-08-04 18:03:31 +0300

    improve the layouts in companies section

--- components/Companies.tsx
+++ components/Companies.tsx
@@ -1,28 +1,31 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { companiesData } from "@/data/data";
-import { PageContainer } from "./Layouts/Container";
+import { Container } from "./Layouts/Container";
 import { InfiniteScrollX } from "./InfiniteScrollX";
+import Section from "./Layouts/Section";
 
 const Companies = () => {
   return (
-    <PageContainer className="py-10">
-      <InfiniteScrollX
-        speed="slow"
-        pauseOnHover={false}
-        listClassName="gap-16 sm:gap-32"
-      >
-        {companiesData.map((company) => (
-          <li key={company.name} className="w-full ">
-            <img
-              src={company.image}
-              alt={company.name}
-              className="h-[85px] w-[85px] lg:h-24 lg:w-24 xl:h-28 xl:w-28 object-contain"
-            />
-          </li>
-        ))}
-      </InfiniteScrollX>
-    </PageContainer>
+    <Section className="pt-16 pb-0">
+      <Container>
+        <InfiniteScrollX
+          speed="slow"
+          pauseOnHover={false}
+          listClassName="gap-16 sm:gap-32"
+        >
+          {companiesData.map((company) => (
+            <li key={company.name} className="w-full ">
+              <img
+                src={company.image}
+                alt={company.name}
+                className="h-[85px] w-[85px] lg:h-24 lg:w-24 xl:h-28 xl:w-28 object-contain"
+              />
+            </li>
+          ))}
+        </InfiniteScrollX>
+      </Container>
+    </Section>
   );
 };
 
